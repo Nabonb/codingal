849ef3b333a5be8c9cf0c8e09e977388f60d8b66
Nabonb <nabobaruanb@gmail.com>
2026-01-23 04:43:28 +0600

    Updated

---
+++ AI Expert  Hands On AI LLMs & Python/Introduction to AI/demo.py
@@ -0,0 +1,12 @@
+import datetime  
+    
+# using now() to get current time  
+time = datetime.datetime.now()  
+    
+# Printing value of now.  
+print ("Time now at greenwich meridian is : ", end = "")   
+print (time)
+
+# print calendar of year 2021
+import calendar
+print("\n", calendar.calendar(2028))
